437286941692d637cd1fdd70fb8f40ad3700c33c
brenermr <brenercaique0806@gmail.com>
2022-03-09 20:50:59 -0300

    DiceRoller

--- app/src/main/java/com/ciandt/diceroller/MainActivity.kt
+++ app/src/main/java/com/ciandt/diceroller/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ciandt.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ciandt.diceroller.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val button = binding.DiceRollerButton
+        val textView = binding.diceRollerText
+        button.text = getString(R.string.buttonRoll)
+
+        button.setOnClickListener {
+            textView.text = rollDice().toString()
+        }
+    }
+    private fun rollDice(): Int {
+        return Random().nextInt(6) + 1
     }
 }
