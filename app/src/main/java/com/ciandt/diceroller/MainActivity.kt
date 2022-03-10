d28166540bbd39cb87d5a47dd501f129c4083569
brenermr <brenercaique0806@gmail.com>
2022-03-09 23:44:09 -0300

    DiceRoller

--- app/src/main/java/com/ciandt/diceroller/MainActivity.kt
+++ app/src/main/java/com/ciandt/diceroller/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ciandt.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.ciandt.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var diceImage:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,20 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val button = binding.DiceRollerButton
-        val imageView = binding.diceRollerImage
+        diceImage = binding.diceRollerImage
 
         button.setOnClickListener {
-            when(rollDice().toString().toInt()){
-                 1-> imageView.setImageResource(R.drawable.dice_1)
-                 2-> imageView.setImageResource(R.drawable.dice_2)
-                 3-> imageView.setImageResource(R.drawable.dice_3)
-                 4-> imageView.setImageResource(R.drawable.dice_4)
-                 5-> imageView.setImageResource(R.drawable.dice_5)
-                else-> imageView.setImageResource(R.drawable.dice_6)
-            }
+            rollDice()
         }
     }
-    private fun rollDice(): Int {
-        return Random().nextInt(6) + 1
+
+    private fun rollDice() {
+        val drawableSet = when (Random().nextInt(6) + 1.toString().toInt()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableSet)
     }
 }
