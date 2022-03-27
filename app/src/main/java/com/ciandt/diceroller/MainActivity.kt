15dc0eed765691f9e61e23f9a6e439b9a0a69d8f
brenermr <brenercaique0806@gmail.com>
2022-03-27 19:29:25 -0300

    Implementation ViewModel and LiveData

--- app/src/main/java/com/ciandt/diceroller/MainActivity.kt
+++ app/src/main/java/com/ciandt/diceroller/MainActivity.kt
@@ -3,12 +3,14 @@ package com.ciandt.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.lifecycle.ViewModelProvider
 import com.ciandt.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var diceImage:ImageView
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,21 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         val button = binding.DiceRollerButton
         diceImage = binding.diceRollerImage
-
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         button.setOnClickListener {
-            rollDice()
+            viewModel.rollDice()
         }
+        viewModel.drawableSet.observe(this, androidx.lifecycle.Observer {
+            binding.diceRollerImage.setImageResource(it)
+        })
     }
 
-    private fun rollDice() {
-        val drawableSet = when (Random().nextInt(6) + 1.toString().toInt()) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableSet)
-    }
+
 }
---
+++ app/src/main/java/com/ciandt/diceroller/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.ciandt.diceroller
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class MainViewModel : ViewModel() {
+    var drawableSet = MutableLiveData<Int>()
+
+    init {
+        drawableSet.value = R.drawable.dice_1
+    }
+
+    fun rollDice() {
+        when (Random().nextInt(6) + 1.toString().toInt()) {
+            1 -> drawableSet.value = R.drawable.dice_1
+            2 -> drawableSet.value  = R.drawable.dice_2
+            3 -> drawableSet.value  =  R.drawable.dice_3
+            4 -> drawableSet.value  =  R.drawable.dice_4
+            5 -> drawableSet.value  =  R.drawable.dice_5
+            else -> drawableSet.value  =  R.drawable.dice_6
+        }
+    }
+}
